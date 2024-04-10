fe725f9ec7009f85789bfef42a38373d61f5db05
cjjeon <hello@cjjeon.com>
2024-04-09 19:33:26 -0700

    Adding console log

--- src/action/getCurrentDateInUnix.ts
+++ src/action/getCurrentDateInUnix.ts
@@ -3,6 +3,7 @@
 import utils from "@/utils";
 
 export default async function () {
+  console.log('getting Current Date In Unix');
   const date = new Date()
   const unix = new Date(date).getTime()
   await utils.sleep(2000)
